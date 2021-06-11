5fcb6b256913aeabb497345a4158d12838f5cd14
Yevhenii Herasymenko <74263798+Zhekager@users.noreply.github.com>
2021-06-11 23:36:23 +0300

    добавляет стили

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 import './sass/main.scss';
-
 import './js/search-countries';
--- src/js/search-countries.js
+++ src/js/search-countries.js
@@ -1,14 +1,19 @@
+
 import renderCountries from '../templates/list-coutries.hbs'
-import renderCountry from '../templates/one-country.hbs'
-import debounce from 'lodash.debounce'
-import fetchCountries from './fetchCountry'
+import renderCountry from '../templates/one-country.hbs';
+import debounce from 'lodash.debounce';
+import fetchCountries from './fetchCountry';
 
 
-import { error } from '@pnotify/core'
-import "@pnotify/core/dist/PNotify.css";
-import { defaultModules } from '@pnotify/core'
-import * as PNotifyBootstrap4 from '@pnotify/bootstrap4'
-// import '@pnotify/bootstrap4/dist/PNotifyBootstrap4.css';
+import { error } from '@pnotify/core';
+// import "@pnotify/core/dist/PNotify.css";
+import { defaultModules } from '@pnotify/core';
+import * as PNotifyBootstrap4 from '@pnotify/bootstrap4';
+import '@pnotify/bootstrap4/dist/PNotifyBootstrap4.css';
+
+
+
+
 
 const containerCountries = document.querySelector('.js-countries');
 const inputNameCountry = document.querySelector('#input-name-country');
@@ -31,20 +36,18 @@ function onSearch() {
 // функция отрисовки контента
 function contentOutput(countries) {
     if (countries.length >= 2 && countries.length <= 10) {
-        containerCountries.innerHTML = renderCountries(countries);
-        // inputNameCountry.value = '';
+        containerCountries.innerHTML = renderCountries(countries);  
     }
     
     if (countries.length === 1) {
         containerCountries.innerHTML = renderCountry(countries);
-        // inputNameCountry.value = '';
     }
 
     if (countries.length > 10) {
-        // console.log('Введите запрос точнее');
         error({
-            title: "Error:",
-            text: "Too many matches found. Please enter a more specific query!"
+    
+            text: "Too many matches found. Please enter a more specific query!",
+            delay: 2100,
         });
     }
-}
+} 
